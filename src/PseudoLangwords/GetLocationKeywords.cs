629a12103549ca2c2b997d761303a20cadfd8c12
Arthri <41360489+Arthri@users.noreply.github.com>
2022-07-24 20:27:41 +0800

    feat: caller functions

---
+++ src/PseudoLangwords/GetLocationKeywords.cs
@@ -0,0 +1,31 @@
+﻿global using static PseudoLangwords.GetLocationKeywords;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PseudoLangwords;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class GetLocationKeywords
+{
+#pragma warning disable IDE1006 // Naming Styles
+    /// <summary>
+    /// Returns the line number where this method is called.
+    /// </summary>
+    /// <param name="lineNumber">Do not specify.</param>
+    /// <returns>The line number where this method is called.</returns>
+    public static int linenum([CallerLineNumber] int lineNumber = 0)
+    {
+        return lineNumber;
+    }
+
+    /// <summary>
+    /// Returns the file where this method is called.
+    /// </summary>
+    /// <param name="filePath">Do not specify.</param>
+    /// <returns>The file where this method is called.</returns>
+    public static string currentfile([CallerFilePath] string filePath = "")
+    {
+        return filePath;
+    }
+#pragma warning restore IDE1006 // Naming Styles
+}
---
+++ src/PseudoLangwords/NameofExpressionKeywords.cs
@@ -0,0 +1,22 @@
+﻿global using static PseudoLangwords.NameofExpressionKeywords;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PseudoLangwords;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class NameofExpressionKeywords
+{
+#pragma warning disable IDE1006 // Naming Styles
+    /// <summary>
+    /// Returns the specified expression's string representation.
+    /// </summary>
+    /// <param name="obj">The expression.</param>
+    /// <param name="expression">Do not specify.</param>
+    /// <returns>The expression's string representation.</returns>
+    public static string nameofexpr(object obj, [CallerArgumentExpression("obj")] string expression = "")
+    {
+        return expression;
+    }
+#pragma warning restore IDE1006 // Naming Styles
+}
