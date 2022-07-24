fedb45fd754ea004fa696c8020254b47c94036ad
Arthri <41360489+Arthri@users.noreply.github.com>
2022-07-24 21:32:59 +0800

    feat: shorten timeonly to time and dateonly to date

--- src/PseudoLangwords/AdditionalTypeKeywords.cs
+++ src/PseudoLangwords/AdditionalTypeKeywords.cs
@@ -2,6 +2,6 @@
 global using complex = System.Numerics.Complex;
 global using bigint = System.Numerics.BigInteger;
 global using datetime = System.DateTime;
-global using timeonly = System.TimeOnly;
-global using dateonly = System.DateOnly;
+global using time = System.TimeOnly;
+global using date = System.DateOnly;
 global using timespan = System.TimeSpan;
