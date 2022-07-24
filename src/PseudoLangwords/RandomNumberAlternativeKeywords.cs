9c1841671b5a5a62a7b8aef4c1fcd1b7688d7352
Arthri <41360489+Arthri@users.noreply.github.com>
2022-07-24 20:09:45 +0800

    docs: add documentation

--- src/PseudoLangwords/RandomNumberAlternativeKeywords.cs
+++ src/PseudoLangwords/RandomNumberAlternativeKeywords.cs
@@ -1,4 +1,4 @@
-﻿global using static PseudoLangwords.RandomNumberAlternativeKeywords;
+global using static PseudoLangwords.RandomNumberAlternativeKeywords;
 using System.Runtime.CompilerServices;
 
 namespace PseudoLangwords;
@@ -7,54 +7,81 @@ public static class RandomNumberAlternativeKeywords
 {
 #pragma warning disable IDE1006 // Naming Styles
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="sbyte.MinValue" /> and less than or equal to <see cref="sbyte.MaxValue" />.
+    /// </summary>
     public static sbyte rint8
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => rsbyte;
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="byte.MinValue" /> and less than or equal to <see cref="byte.MaxValue" />.
+    /// </summary>
     public static byte ruint8
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => rbyte;
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="short.MinValue" /> and less than or equal to <see cref="short.MaxValue" />.
+    /// </summary>
     public static short rint16
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => rshort;
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="ushort.MinValue" /> and less than or equal to <see cref="ushort.MaxValue" />.
+    /// </summary>
     public static ushort ruint16
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => rushort;
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="int.MinValue" /> and less than or equal to <see cref="int.MaxValue" />.
+    /// </summary>
     public static int rint32
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => rint;
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="uint.MinValue" /> and less than or equal to <see cref="uint.MaxValue" />.
+    /// </summary>
     public static uint ruint32
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => ruint;
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="long.MinValue" /> and less than or equal to <see cref="long.MaxValue" />.
+    /// </summary>
     public static long rint64
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => rlong;
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="ulong.MinValue" /> and less than or equal to <see cref="ulong.MaxValue" />.
+    /// </summary>
     public static ulong ruint64
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => rulong;
     }
 
+    /// <summary>
+    /// <inheritdoc cref="Random.NextSingle" path="/returns" />.
+    /// </summary>
     public static float rsingle
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
--- src/PseudoLangwords/RandomNumberKeyword.cs
+++ src/PseudoLangwords/RandomNumberKeyword.cs
@@ -10,42 +10,63 @@ internal static class RandomNumberKeyword
 {
 #pragma warning disable IDE1006 // Naming Styles
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="sbyte.MinValue" /> and less than or equal to <see cref="sbyte.MaxValue" />.
+    /// </summary>
     public static sbyte rsbyte
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => (sbyte)Random.Shared.Next(sbyte.MinValue, sbyte.MaxValue + 1);
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="byte.MinValue" /> and less than or equal to <see cref="byte.MaxValue" />.
+    /// </summary>
     public static byte rbyte
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => (byte)Random.Shared.Next(byte.MinValue, byte.MaxValue + 1);
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="short.MinValue" /> and less than or equal to <see cref="short.MaxValue" />.
+    /// </summary>
     public static short rshort
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => (short)Random.Shared.Next(short.MinValue, short.MaxValue + 1);
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="ushort.MinValue" /> and less than or equal to <see cref="ushort.MaxValue" />.
+    /// </summary>
     public static ushort rushort
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => (ushort)Random.Shared.Next(ushort.MinValue, ushort.MaxValue + 1);
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="int.MinValue" /> and less than or equal to <see cref="int.MaxValue" />.
+    /// </summary>
     public static int rint
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => (int)Random.Shared.NextInt64(int.MinValue, (long)int.MaxValue + 1);
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="uint.MinValue" /> and less than or equal to <see cref="uint.MaxValue" />.
+    /// </summary>
     public static uint ruint
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => (uint)Random.Shared.NextInt64(uint.MinValue, (long)uint.MaxValue + 1);
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="long.MinValue" /> and less than or equal to <see cref="long.MaxValue" />.
+    /// </summary>
     public static long rlong
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -58,6 +79,9 @@ internal static class RandomNumberKeyword
         }
     }
 
+    /// <summary>
+    /// A random number bigger than or equal to <see cref="ulong.MinValue" /> and less than or equal to <see cref="ulong.MaxValue" />.
+    /// </summary>
     public static ulong rulong
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -70,12 +94,18 @@ internal static class RandomNumberKeyword
         }
     }
 
+    /// <summary>
+    /// <inheritdoc cref="Random.NextSingle" path="/returns" />.
+    /// </summary>
     public static float rfloat
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => Random.Shared.NextSingle();
     }
 
+    /// <summary>
+    /// <inheritdoc cref="Random.NextDouble" path="/returns" />.
+    /// </summary>
     public static double rdouble
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
