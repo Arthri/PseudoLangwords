fe509797e4d763446bfb2bdb719c3ffa9bffcf98
Arthri <41360489+Arthri@users.noreply.github.com>
2022-07-25 08:46:48 +0800

    feat: bool alternative literals

---
+++ src/PseudoLangwords/BoolAlternativeLiterals.cs
@@ -0,0 +1,58 @@
+﻿global using static PseudoLangwords.BoolAlternativeLiterals;
+using System.ComponentModel;
+
+namespace PseudoLangwords;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class BoolAlternativeLiterals
+{
+    /// <summary>
+    /// A boolean value, <see langword="false"/>.
+    /// </summary>
+    public const bool False = false;
+
+    /// <summary>
+    /// A boolean value, <see langword="true"/>.
+    /// </summary>
+    public const bool True = true;
+
+    /// <summary>
+    /// A boolean value, <see langword="false"/>.
+    /// </summary>
+    public const bool Off = false;
+
+    /// <summary>
+    /// A boolean value, <see langword="true"/>.
+    /// </summary>
+    public const bool On = true;
+
+    /// <summary>
+    /// A boolean value, <see langword="false"/>.
+    /// </summary>
+    public const bool off = false;
+
+    /// <summary>
+    /// A boolean value, <see langword="true"/>.
+    /// </summary>
+    public const bool on = true;
+
+    /// <summary>
+    /// A boolean value, <see langword="false"/>.
+    /// </summary>
+    public const bool No = false;
+
+    /// <summary>
+    /// A boolean value, <see langword="true"/>.
+    /// </summary>
+    public const bool Yes = true;
+
+    /// <summary>
+    /// A boolean value, <see langword="false"/>.
+    /// </summary>
+    public const bool no = false;
+
+    /// <summary>
+    /// A boolean value, <see langword="true"/>.
+    /// </summary>
+    public const bool yes = true;
+}
