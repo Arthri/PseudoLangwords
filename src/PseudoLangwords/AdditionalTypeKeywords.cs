5a665c2a775371dbf52f99dd7ed688c65329e9f4
Arthri <41360489+Arthri@users.noreply.github.com>
2022-07-25 11:42:47 +0800

    feat: reorganize

--- src/PseudoLangwords/AdditionalTypeKeywords.cs
+++ src/PseudoLangwords/AdditionalTypeKeywords.cs
@@ -1,4 +1,5 @@
-﻿global using boolean = System.Boolean;
+﻿#region Integral Types
+
 global using int8 = System.SByte;
 global using uint8 = System.Byte;
 global using int16 = System.Int16;
@@ -7,10 +8,30 @@ global using int32 = System.Int32;
 global using uint32 = System.UInt32;
 global using int64 = System.Int64;
 global using uint64 = System.UInt64;
-global using single = System.Single;
+
+#endregion
+
+#region Floating Point Types
+
 global using half = System.Half;
+global using fp16 = System.Half;
+global using float16 = System.Half;
+global using single = System.Single;
+global using fp32 = System.Single;
+global using float32 = System.Single;
+global using fp64 = System.Double;
+global using float64 = System.Double;
+
+#endregion
+
+#region Other Numeric Types
+
 global using complex = System.Numerics.Complex;
 global using bigint = System.Numerics.BigInteger;
+
+#endregion
+
+global using boolean = System.Boolean;
 
 global using datetime = System.DateTime;
 global using time = System.TimeOnly;
